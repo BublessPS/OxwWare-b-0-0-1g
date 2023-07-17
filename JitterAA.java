7b7f26a1eafc4487ba92de09cd47905d4acf48b1
BublessPS <113979445+BublessPS@users.noreply.github.com>
2023-07-17 13:41:07 +0300

    JitterAA.java
    Prictic

---
+++ JitterAA.java
@@ -0,0 +1,9 @@
+UI.AddCheckBox(AA);
+var skeet1 == true;
+ function(skd)
+   if skeet1 = true:
+UI.SetValue(“GUI”, “Movement”, “Flight”, “speed”) 15
+  skeet1 == false;
+  else skeet1 = false
+UI.SetValue(“GUI”, “Movement”, “Flight”, “speed”) 5
+  skeet1 == true;
